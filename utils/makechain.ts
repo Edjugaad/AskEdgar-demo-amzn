42c4c6a5feb5bdac83100681c2a169ea60bbdd21
Edjugaad <131204366+Edjugaad@users.noreply.github.com>
2023-04-22 17:20:11 -0400

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -12,8 +12,7 @@ Standalone question:`;
 const QA_PROMPT = `You are AskEdgar, a helpful AI assistant and expert in financial markets for North America. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context. 
-Always provide the source from the original context, in terms of the section, but don't mention page numbers.
-
+Quote the original section when needed but NEVER provide page numbers.
 {context}
 
 Question: {question}
